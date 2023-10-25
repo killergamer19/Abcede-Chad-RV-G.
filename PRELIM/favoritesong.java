56af96bd45ce6828364a21a4ed214ddd788da4cf
killergamer19 <120266992+killergamer19@users.noreply.github.com>
2023-10-25 06:05:32 +0100

    Add files via upload

---
+++ PRELIM/favoritesong.java
@@ -0,0 +1,14 @@
+/*
+   Chad RV G. Abcede
+*/
+
+class favoritesong {
+  public static void main(String[] args) {
+    System.out.println("Say That You're Mine Song by JSO and VALENTINE");
+    System.out.println();
+    System.out.println("When you're here I realized");
+    System.out.println("I'll be the one to testify");
+    System.out.println("Baby");
+    System.out.println("Say that you're mine tonight");
+  }
+}
